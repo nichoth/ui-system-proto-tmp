3af7d1a66662272a01e3291316083f89bb0e3401
Nick Thomas <nichoth@gmail.com>
2017-10-29 16:43:54 -0700

    tests for counter

--- examples/async-counter/test.js
+++ examples/async-counter/test.js
@@ -1,18 +1,41 @@
-var assert = require('assert')
-var Counter = require('./counter')
+var test = require('tape')
+var State = require('./state')
+var Counter = State.Counter
+var Effects = require('./effects')
 
-var counter = Counter()
+test('counter', function (assert) {
+    var counter = Counter()
+    assert.equal(counter.count, 0)
+    assert.equal(Counter.inc(counter)().count, 1)
+    assert.equal(Counter.dec(counter)().count, 0)
+    assert.end()
+})
 
-assert.equal(counter.count, 0)
-assert.equal(Counter.inc(counter)().count, 1)
-assert.equal(Counter.dec(counter)().count, 0)
+test('effects', function (t) {
+    var state = State()
+    t.plan(5)
 
-var effects = Counter.effects(httpEcho)
-effects.asyncInc(counter)()
+    var effects = Effects() // would need to give mock IO functions here
+    var expect = [0, 1]
+    var i = 0
+    state.counter.on('count', function () {
+        t.equal(state.counter.count, expect[i++])
+    })
 
-function httpEcho (data, cb) {
-    setTimeout(function () {
-        cb(null, data)
-    }, 1000)
-}
+    function expectResolving (state) {
+        return [
+            [state.requests.resolving[0], undefined],
+            [(state.requests.resolving[0] || {}).type, 'inc'],
+            [state.requests.resolving[0], undefined]
+        ]
+    }
+
+    var _i = 0
+    state.requests.on('resolving', function () {
+        t.equal.apply(t, expectResolving(state)[_i++])
+    })
+
+    effects.asyncInc(state)()
+})
+
 
